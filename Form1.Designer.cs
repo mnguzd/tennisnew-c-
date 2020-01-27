b561083d149a32d693a4a56c2cdb5d43954ab9c6
mnguzd <roman.kioroglo@gmail.com>
2020-01-27 23:25:14 +0200

    last one

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace TenisNew
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::TenisNew.Resource1.START;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(345, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 92);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 1000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Firebrick;
+            this.label1.Location = new System.Drawing.Point(410, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 116);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans Unicode", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.GreenYellow;
+            this.label2.Location = new System.Drawing.Point(28, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 34);
+            this.label2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TenisNew.Properties.Resources.map;
+            this.ClientSize = new System.Drawing.Size(918, 508);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+namespace TenisNew
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap Ball = Resource1.ebalo;
+        public Bitmap Wall = Resource1.wall;
+        int x = 50; int y = 50; int z = 422; int j = 473;
+        int x1 = 3; int y1 = 3;int width2; int i;
+        Random pand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint, true);
+            UpdateStyles(); Rand(); Interface();
+            width2 = 150;
+        }
+        public void Rand()
+        {
+            int napr = pand.Next(0, 2);
+            x1 = napr == 0 ? pand.Next(3, 5) : -pand.Next(3, 5); y1 = pand.Next(3, 5);
+            x = pand.Next(100, 800); y = pand.Next(100, 200);
+            Refresh();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (z+width2 > 910)
+                timer2.Enabled = false;
+            if(z<8)
+                timer3.Enabled = false;
+            Refresh();
+            x += x1; y += y1;
+            if (x + 16 > z - 7 && x + 16 < z + 7 + width2 && y + 32 > 470)
+            {
+                width2 = width2 > 20 ? width2 - 10 : width2;
+                y1 = x1 > 0 && y1 > 0 ? -y1 : x1 < 0 && y1 > 0 ? -y1 : -y1;
+            }
+            if (x + 32 > 900)
+                x1 = x1 > 0 && y1 < 0 ? -x1 : x1 > 0 && y1 > 0 ? -x1 : -x1;
+            if (y < 3)
+                y1 = x1 < 0 && y1 < 0 ? -y1 : x1 > 0 && y1 < 0 ? -y1 : -y1;
+            if (x < 3)
+                x1 = x1 < 0 && y1 > 0 ? -x1 : x1 < 0 && y1 < 0 ? -x1 : -x1;
+            if (y+32> 500)
+                Interface();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)Keys.D)
+                    timer2.Enabled = true;
+            if (e.KeyValue == (char)Keys.A)
+                timer3.Enabled = true;
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            z += 6;
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            z -= 6;
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            timer2.Enabled = false; timer3.Enabled = false;
+        }
+        void Interface()
+        {
+            timer1.Enabled = false;
+            button1.Show();
+            Rand();
+            label1.Hide();
+            i = 3; z = 422; j = 473;
+        }
+        void Game()
+        {
+            width2 = 150;
+            button1.Hide();
+            timer1.Enabled = true;
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+                Graphics g = e.Graphics;
+                g.DrawImage(Ball, new Rectangle(x, y, 32, 32));
+                g.DrawImage(Wall, new Rectangle(z, j, width2, 25));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer4.Enabled = true; button1.Hide();
+        }
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+                label1.Text = i.ToString(); label1.Show();
+                i--;
+                if (i == -1)
+                {
+                    label1.Hide();
+                    Game();
+                    timer4.Enabled = false;
+                }
+        }
+    }
+}
